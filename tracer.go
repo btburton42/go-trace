577d29016604b2c08eed6b91497a62c58c140776
Tyler Burton <btburton42@gmail.com>
2017-04-03 10:40:11 -0700

    first touch commit of tracer module

---
+++ tracer.go
@@ -0,0 +1,31 @@
+package trace
+
+import (
+	"io"
+	"fmt"
+)
+
+type tracer struct {
+	out io.Writer
+}
+
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type nilTracer struct {}
+
+func (t *nilTracer) Trace (a ...interface{}){}
+
+func Off() Tracer {
+	return &nilTracer{}
+}
